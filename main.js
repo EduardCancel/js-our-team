613791453be42ddb8c6aad9b4876dcac1313b783
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-24 17:13:04 +0200

    changes html

--- main.js
+++ main.js
@@ -1,62 +1,47 @@
 const formEl = document.getElementById("form-control");
 const cardsEl = document.getElementById("cards");
 const teamMembers = [
-{
+  {
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
     img: "img/male1.png",
-},
+  },
 
-{
+  {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
     img: "img/female1.png",
-},
+  },
 
-{
+  {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
     img: "img/male2.png",
-},
+  },
 
-{
+  {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
     img: "img/female2.png",
-},
+  },
 
-{
+  {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
     img: "img/male3.png",
-},
+  },
 
-{
+  {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
     img: "img/female3.png",
-},
+  },
 ];
 
-
-// Cicle for the array
-
-for (let i = 0; i < teamMembers.length; i++){
-    const thisMembersObj = teamMembers[i]
-    console.log(teamMembersObj);
-
-    // Create html markup
-    const markUp = getMarkup(thisMembersObj)
-
-    cardsEl.innerHTML += markUp;
-}
-
 //  Function section
-
-
