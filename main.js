a5bb2746b324d76bf13dbfc197576603c0392a8b
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-24 17:57:51 +0200

    add code card html

--- main.js
+++ main.js
@@ -44,4 +44,37 @@ const teamMembers = [
   },
 ];
 
-//  Function section
+
+function TeamMember(membersArray, domEl) {
+    for (i = 0; i < membersArray.length; i++){
+        const thisMember = membersArray[i]
+        console.log(thisMember);
+    }
+
+    const markup = memberMarkup(thisMember)
+    console.log(markup);
+
+    domEl.innerHtml += markup
+}
+
+TeamMember(teamMembers, cardsEl)
+
+
+function memberMarkup(memberObj){
+
+    const {name, role, email, image} = memberObj
+    console.log(name, role, email, image);
+
+    const markup = `
+    <div class="card col-4">
+        <img src="${image}" style="width: 100px;" alt="" class="card-mg-top">
+        <div class="card-body bg-dark text-light">
+            <h3>"${name}"</h3>
+            <p>"${role}"</p>
+            <p class="text-primary">"${email}"</p>
+        </div>
+    </div>`
+
+console.log(markup);
+return markup
+}
